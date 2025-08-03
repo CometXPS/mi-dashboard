d32e7330b14d3d732f5c21eaa15c0cb0cba11b27
CometXPS <contreras.001@gmail.com>
2025-08-02 23:50:55 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// =================================================================================
+// PASO CLAVE: CONFIGURACIÓN DE FIREBASE
+// Ve a la configuración de tu proyecto en Firebase, registra una app web y
+// pega aquí el objeto de configuración COMPLETO que te proporcionan.
+// =================================================================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLExGhrpjSurJcjMIYLQnS_wRYvXhAq6w",
+  authDomain: "dashboard-divisas.firebaseapp.com",
+  projectId: "dashboard-divisas",
+  storageBucket: "dashboard-divisas.firebasestorage.app",
+  messagingSenderId: "147054939234",
+  appId: "1:147054939234:web:73c431157858cb69ef553b"
+};
