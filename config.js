e2528284ec34302e7433d7bec39b5136894f3b0f
CometXPS <contreras.001@gmail.com>
2025-08-04 12:51:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // =================================================================================
-// PASO CLAVE: CONFIGURACIÓN DE FIREBASE
+// 1. PASO CLAVE: CONFIGURACIÓN DE FIREBASE
 // Ve a la configuración de tu proyecto en Firebase, registra una app web y
 // pega aquí el objeto de configuración COMPLETO que te proporcionan.
 // =================================================================================
@@ -11,4 +11,9 @@ export const firebaseConfig = {
   storageBucket: "dashboard-divisas.firebasestorage.app",
   messagingSenderId: "147054939234",
   appId: "1:147054939234:web:73c431157858cb69ef553b"
+
 };
+
+// 2. Clave API de PolygonScan
+// Reemplaza esta clave con la que obtuviste de https://polygonscan.com/myapikey
+export const polygonScanApiKey = "CN3F3AEDSS8F8I6CUNVIURXDC71QB485MC";
